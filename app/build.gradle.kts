de54fd429c2a4b6b1940f681a6c04eb80cb64e6e
Priyanshu <idealpriyanshu@gmail.com>
2025-09-15 16:18:48 +0530

    Add screenshot, update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "0.2.3"
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {useSupportLibrary = true }
